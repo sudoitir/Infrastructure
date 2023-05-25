7b63e6dc0622bc5d668ffb59e20d46e1963f822d
Mahdi Amirabdolahi <sudoit.ir@gmail.com>
2023-05-25 12:25:29 +0330

    [INIT] Initial commit

---
+++ base/.mvn/src/test/java/ir/sudoit/base/BaseApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.sudoit.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
