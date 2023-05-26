758f529c4a9953c1fdc3838a789586aead55970c
Mahdi Amirabdolahi <sudoit.ir@gmail.com>
2023-05-26 13:17:26 +0330

    [FEATURE] Add StringEncryptorConfig

---
+++ base/src/main/java/encrypt/StringEncryptorConfig.java
@@ -0,0 +1,29 @@
+package encrypt;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StringEncryptorConfig {
+
+
+    @Bean ("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        var encryptor = new PooledPBEStringEncryptor();
+        var config = new SimpleStringPBEConfig();
+        config.setPassword("123");
+        config.setAlgorithm("PBEWITHHMACSHA512ANDAES_256");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+
+}
