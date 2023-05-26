b934821928a7d76cb745d7e865c981cc0a6978d2
Mahdi Amirabdolahi <sudoit.ir@gmail.com>
2023-05-26 17:39:58 +0330

    [FEATURE] Fix Config Server props

--- base/src/main/java/ir/sudoit/base/encrypt/StringEncryptorConfig.java
+++ base/src/main/java/ir/sudoit/base/encrypt/StringEncryptorConfig.java
@@ -1,4 +1,4 @@
-package encrypt;
+package ir.sudoit.base.encrypt;
 
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
@@ -14,7 +14,7 @@ public class StringEncryptorConfig {
     public StringEncryptor stringEncryptor() {
         var encryptor = new PooledPBEStringEncryptor();
         var config = new SimpleStringPBEConfig();
-        config.setPassword("123");
+        config.setPassword("753357");
         config.setAlgorithm("PBEWITHHMACSHA512ANDAES_256");
         config.setKeyObtentionIterations("1000");
         config.setPoolSize("1");
